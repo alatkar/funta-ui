02015b4d843d0905a118d4c3ea420ec88f4aae6e
Atul Alatkar <atalatk@microsoft.com>
2020-07-29 23:00:33 -0700

    Uncommited changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Container, Segment, Grid, Image} from "semantic-ui-react";
+
+import Header from './components/Header/Header';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
+import LoginForm from './components/LoginForm/LoginForm';
+import AlertComponent from './components/AlertComponent/AlertComponent';
+import NewsFeed from './components/NewsFeed/NewsFeed';
+import Login from './components/Login/Login';
 import './App.css';
 
 function App() {
+  const [title,updateTitle] = useState(null);
+  const [errorMessage, updateErrorMessage] = useState(null);
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={title || "Register"}/>
+      <div className="container d-flex align-items-center flex-column">
+        <Switch>
+          <Route path="/" exact={true}>
+          <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+          </Route>
+          <Route path="/register">
+            <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+          </Route>
+          <Route path="/login">
+            {/* <LoginForm showError={updateErrorMessage} updateTitle={updateTitle}/> */}
+            <Login showError={updateErrorMessage} updateTitle={updateTitle}></Login>
+          </Route>
+          <Route path ="/home">
+
+            <NewsFeed showError={updateErrorMessage} updateTitle={updateTitle}></NewsFeed>
+
+          </Route>
+        </Switch>
+        <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage}></AlertComponent>
+
+
+      </div>
     </div>
+    </Router>
   );
 }
 
